b7ea1050f3e28fc3d5ae371fe7f8ac5e9615610c
Arda Demirkıran <kali@kali.local>
2022-08-08 01:44:41 +0300

    Add Session.java file

---
+++ src/main/java/com/tempims/tempims/Session.java
@@ -0,0 +1,57 @@
+package com.tempims.tempims;
+
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Base64;
+
+public class Session {
+    public static String username;
+
+    public static int checkLogin(String usernameInput, String passwordInput){
+
+        String hashedPasswordDB = "";  // This part will be accessed from sql database by using "username"
+        String hashedPasswordInput = hashPassword(passwordInput);
+
+        if (hashedPasswordDB.equals(hashedPasswordInput)){
+            username = usernameInput; // This line assigns username of the current user to static "username" variable for action recording.
+            return 1;
+        } else {
+            return 0;
+        }
+    }
+
+    public int checkSignUp(String usernameInput, String passwordInput1, String passwordInput2){
+
+        //sql part to check username and  return -2 if username already exists
+
+
+        if (passwordInput1.equals("") || passwordInput2.equals("") || usernameInput.equals("")){ // these parts can not be empty if so returns -1
+            return -1;
+        }
+
+
+        if (!passwordInput1.equals(passwordInput2)){ // in case two password inputs doesn't match if so return 0
+            return 0;
+        }
+
+
+        // sql part to put new user to database and returns 1
+
+        return 1;
+
+
+    }
+
+
+    private static String hashPassword(String password){ //password hashing method
+        byte[] bytesOfPassword = password.getBytes(StandardCharsets.UTF_8);
+        byte[] md5Digest = new byte[0];
+        try{
+            md5Digest = MessageDigest.getInstance("MD5").digest(bytesOfPassword);
+        }catch (NoSuchAlgorithmException e){
+            return null;
+        }
+        return Base64.getEncoder().encodeToString(md5Digest);
+    }
+}
